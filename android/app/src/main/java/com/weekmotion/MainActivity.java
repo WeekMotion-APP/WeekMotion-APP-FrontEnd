a27be6f2a188aa425667e18d0864e7deb203d455
iuesver <87600354+iuesver@users.noreply.github.com>
2023-06-30 18:11:09 +0900

    feat : add splash screen / app icon

--- android/app/src/main/java/com/weekmotion/MainActivity.java
+++ android/app/src/main/java/com/weekmotion/MainActivity.java
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,9 +14,10 @@ public class MainActivity extends ReactActivity {
    * rendering of the component.
    */
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
   @Override
   protected String getMainComponentName() {
     return "WeekMotion"; 
